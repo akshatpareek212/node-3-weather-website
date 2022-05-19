de26dd0c672123afb45d633185caf4caf9ced2e3
Akshat Pareek <akshat.pareek@wedigtech.com>
2022-05-19 12:03:37 +0530

    Update forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,6 +5,7 @@ const forecast = (latitude,longitude,callback) => {
     url=`http://api.weatherstack.com/current?access_key=3d6e1fc6de6415f625fd55b96b3b43cb&query=${latitude},${longitude}`
 
     request({url,json:true},(error,{body}) => {
+        console.log(body);
     if(error){
         callback("Can't connect to network");
     }
@@ -13,7 +14,7 @@ const forecast = (latitude,longitude,callback) => {
     }
     else {
     callback(undefined, 
-        `${body.current.weather_descriptions}. It is ${body.current.temperature} degrees out, but feels like ${body.current.feelslike} degrees out.`)
+        `${body.current.weather_descriptions}. It is ${body.current.temperature} degrees out, but feels like ${body.current.feelslike} degrees out.The humidity is ${body.current.humidity}`)
     }
 })
 
